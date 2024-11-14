d695e99867f1e986e969ebdca35f5d37758d1637
Miguel Romero <mrm040@bucknell.edu>
2024-11-14 12:02:15 -0500

    Sensor Testing

---
+++ sensorTest.py
@@ -0,0 +1,44 @@
+import machine
+import time
+
+# sensor1 = machine.Pin(4,mode = machine.Pin.IN)
+
+
+# doubleSensor1 = machine.Pin(2,mode = machine.Pin.OUT)
+# doubleSensor2 = machine.Pin(3,mode = machine.Pin.OUT)
+
+# sensor1.on()
+# doubleSensor2.on()
+# doubleSensor1.on()
+
+
+# print(doubleSensor1.value())
+# print(doubleSensor2.value())
+# print(sensor1.value())
+
+
+def HCSRSensor(trigger_pin, echo_pin, ):
+    trigger = machine.Pin(trigger_pin, mode = machine.Pin.OUT,pull = None)
+    echo = machine.Pin(echo_pin, mode = machine.Pin.IN,pull = None )
+
+    echo_timeout_us=500*2*30
+
+    trigger.value(0)
+    trigger.value(1)
+    # Send a 10us pulse.
+    time.sleep_us(10)
+    trigger.value(0)
+
+    pulse_time = machine.time_pulse_us(echo, 1, echo_timeout_us)
+
+    mm  = pulse_time * 100 // 582
+    cms = (pulse_time / 2) / 29.1
+
+    ## Distance in millimeters
+    print(mm)
+
+    ## Distance in centimeter
+    print(cms)
+
+
+HCSRSensor(17,16)
--- test.py
+++ test.py
@@ -4,22 +4,22 @@ print("Hello World")
 
 
 
-# import machine
-# import time
+import machine
+import time
 
 
 print("hello")
 
-# M2A = machine.Pin(10)
-# M2B = machine.Pin(11)
+M2A = machine.Pin(10)
+M2B = machine.Pin(11)
 
-# M1A = machine.PWM(machine.Pin(8),freq = 50, duty_u16 = 65535)
-# M1B = machine.PWM(machine.Pin(9), freq = 50, duty_u16 = 0)
-# M2A = machine.PWM(machine.Pin(10), freq = 50, duty_u16 = 65535)
-# M2B = machine.PWM(machine.Pin(11), freq = 50, duty_u16 = 0)
+M1A = machine.PWM(machine.Pin(8),freq = 50, duty_u16 = 0)
+M1B = machine.PWM(machine.Pin(9), freq = 50, duty_u16 = int(65535 * 0.50))
+M2A = machine.PWM(machine.Pin(10), freq = 50, duty_u16 = int(65535 * 0.50))
+M2B = machine.PWM(machine.Pin(11), freq = 50, duty_u16 = 0)
 
 
-# M1A 
-# M1B
-# M2A
-# M2B
+M1A 
+M1B
+M2A
+M2B
