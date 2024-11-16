7262a3b6cae4f5d12baf9ee57da3cb41e64e56e9
Miguel Romero <mrm040@bucknell.edu>
2024-11-16 17:36:24 -0500

    Sensor changes

--- sensorTest.py
+++ sensorTest.py
@@ -2,11 +2,11 @@ import machine
 import time
 
 sensor1 = machine.Pin(4)
-print(sensor1.value())
 
 
-doubleSensor1 = machine.Pin(2)
-doubleSensor2 = machine.Pin(3)
+
+doubleSensor1 = machine.Pin(2, machine.Pin.IN)
+doubleSensor2 = machine.Pin(3, machine.Pin.IN)
 
 # sensor1.on()
 # doubleSensor2.on()
@@ -15,32 +15,33 @@ doubleSensor2 = machine.Pin(3)
 
 print(doubleSensor1.value())
 print(doubleSensor2.value())
+print(sensor1.value())
 # print(sensor1.value())
 
 
-def HCSRSensor(trigger_pin, echo_pin, ):
-    trigger = machine.Pin(trigger_pin, mode = machine.Pin.OUT,pull = None)
-    echo = machine.Pin(echo_pin, mode = machine.Pin.IN,pull = None )
+# def HCSRSensor(trigger_pin, echo_pin, ):
+#     trigger = machine.Pin(trigger_pin, mode = machine.Pin.OUT,pull = None)
+#     echo = machine.Pin(echo_pin, mode = machine.Pin.IN,pull = None )
 
-    echo_timeout_us=500*2*30
+#     echo_timeout_us=500*2*30
 
-    trigger.value(0)
-    trigger.value(1)
-    # Send a 10us pulse.
-    time.sleep_us(10)
-    trigger.value(0)
+#     trigger.value(0)
+#     trigger.value(1)
+#     # Send a 10us pulse.
+#     time.sleep_us(10)
+#     trigger.value(0)
 
-    pulse_time = machine.time_pulse_us(echo, 1, echo_timeout_us)
+#     pulse_time = machine.time_pulse_us(echo, 1, echo_timeout_us)
 
-    mm  = pulse_time * 100 // 582
-    cms = (pulse_time / 2) / 29.1
+#     mm  = pulse_time * 100 // 582
+#     cms = (pulse_time / 2) / 29.1
 
-    ## Distance in millimeters
-    print(mm)
+#     ## Distance in millimeters
+#     print(mm)
 
-    ## Distance in centimeter
-    print(cms)
+#     ## Distance in centimeter
+#     print(cms)
 
 
-while True:
-    HCSRSensor(17,16)
+# while True:
+#     HCSRSensor(17,16)
