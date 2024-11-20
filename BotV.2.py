8b51093cdfb59885c9ea4307428dfc93c467ed4a
Miguel Romero <mrm040@bucknell.edu>
2024-11-20 14:03:31 -0500

    botV.2

--- BotV.2.py
+++ BotV.2.py
@@ -1,44 +1,64 @@
 import machine
-import utime
 import time
-class Bot:
+class bot:
     def __init__(self, **kwargs):
-        # print(kwargs)
-        # setup ultrasound sensor
-        self.trig = machine.Pin(kwargs["trig_pin"], machine.Pin.OUT)
-        self.echo = machine.Pin(kwargs["echo_pin"], machine.Pin.IN)
-        
-        #Gives value of left sensor 
-        self.left = machine.Pin(27, machine.Pin.IN)
-        self.right = machine.Pin(26, machine.Pin.IN)
-        #Gives value of right sensor 
-
-        self.singleSensor = machine.Pin(4)
         # Setup DC Motor pins
-        self.M1A = machine.PWM(machine.Pin(kwargs["M1A"]))
-        self.M1B = machine.PWM(machine.Pin(kwargs["M1B"]))
-        self.M2A = machine.PWM(machine.Pin(kwargs["M2A"]))
-        self.M2B = machine.PWM(machine.Pin(kwargs["M2B"]))
-        self.M1A.freq(50)
-        self.M1B.freq(50)
-        self.M2A.freq(50)
-        self.M2B.freq(50)
+        self.M1A = machine.PWM(machine.Pin(kwargs["M1A"]), freq=50, duty_u16=0)
+        self.M1B = machine.PWM(machine.Pin(kwargs["M1B"]), freq=50, duty_u16=0)
+        self.M2A = machine.PWM(machine.Pin(kwargs["M2A"]), freq=50, duty_u16=0)
+        self.M2B = machine.PWM(machine.Pin(kwargs["M2B"]), freq=50, duty_u16=0)
+        
+        self.left = machine.Pin(kwargs["left"], machine.Pin.IN)
+        self.right = machine.Pin(kwargs["right"], machine.Pin.IN)
+
+        self.A = machine.Pin(kwargs["A"], machine.Pin.IN)
+        self.B = machine.Pin(kwargs["B"], machine.Pin.IN)
 
     def read_line(self):
         return self.left.value(), self.right.value()
 
-
-    def leftRotate(self, speed = 0.3):
+    def rotate(self, speed = 0.3):
         self.M1A.duty_u16(0)     # Duty Cycle must be between 0 until 65535
         self.M1B.duty_u16(int(speed * 65535))
         self.M2A.duty_u16(int(speed * 65535))
         self.M2B.duty_u16(0)
 
-    def rightRotate(self,speed = 0.3):
-        self.M1A.duty_u16(int(speed * 65535))   # Duty Cycle must be between 0 until 65535
+    def turnleft(self, amount_u16 = 0x2000):
+        # turn left by increasing the speed of the right motor and decreasing the speed of the left motor
+        # assumes we are going forward.
+        self.M1A.duty_u16(self.M1A.duty_u16())     # Duty Cycle must be between 0 until 65535
+        self.M1B.duty_u16(self.M1B.duty_u16())
+
+        if self.M2B.duty_u16() == 0:
+            # reverse
+            self.M2A.duty_u16(min(0xffff,self.M2A.duty_u16() + amount_u16))
+            self.M2B.duty_u16(0)
+        else:
+            # forward
+            self.M2A.duty_u16(self.M2A.duty_u16())
+            self.M2B.duty_u16(max(0,self.M2B.duty_u16() - amount_u16))
+
+    def turnright(self, amount_u16 = 0x2000):
+        # turn left by increasing the speed of the right motor and decreasing the speed of the left motor
+        # assumes we are going forward.
+
+        if self.M1B.duty_u16() == 0:
+            # reverse
+            self.M1A.duty_u16(min(0xffff,self.M1A.duty_u16() + amount_u16))
+            self.M1B.duty_u16(0)
+        else:
+            # forward
+            self.M1A.duty_u16(self.M1A.duty_u16())     # Duty Cycle must be between 0 until 65535
+            self.M1B.duty_u16(max(0,self.M1B.duty_u16() - amount_u16))
+
+        self.M2A.duty_u16(self.M2A.duty_u16())
+        self.M2B.duty_u16(self.M2B.duty_u16())
+
+    def stop(self):
+        self.M1A.duty_u16(0)     # Duty Cycle must be between 0 until 65535
         self.M1B.duty_u16(0)
         self.M2A.duty_u16(0)
-        self.M2B.duty_u16(int(speed * 65535))
+        self.M2B.duty_u16(0)
 
     def fwd(self, speed = 0.3):
         self.M1A.duty_u16(0)     # Duty Cycle must be between 0 until 65535
@@ -57,59 +77,44 @@ class Bot:
         self.M1B.duty_u16(65535)
         self.M2A.duty_u16(65535)
         self.M2B.duty_u16(65535)
-
-    def read_distance(self, timeout=100):
-        """Reads distance from an HC-SR04P distance sensor.
         
-        2cm - 400cm range
+def main():
+    conf = {
+        "M1A": 8,
+        "M1B": 9,
+        "M2A": 10,
+        "M2B": 11,
+        "left": 2,
+        "right": 3,
+        "A": 20,
+        "B": 21
+    }
+    b = bot(**conf)
 
-        Args:
-            trig_pin: The GPIO pin connected to the trigger pin of the sensor.
-            echo_pin: The GPIO pin connected to the echo pin of the sensor.
-            timeout: The maximum time (in milliseconds) to wait for a response from the sensor.
-
-        Returns:
-            The measured distance in centimeters, or None if no response was received within the timeout.
-            
-        Gemini
-        """
-
-        # Trigger the sensor
-        self.trig.value(0)
-        utime.sleep_us(2)
-        self.trig.value(1)
-        utime.sleep_us(10)
-        self.trig.value(0)
-
-        # Wait for the echo signal
-        start_time = utime.ticks_us()
-        while self.echo.value() == 0:
-            signaloff = utime.ticks_us()
-            if utime.ticks_diff(signaloff, start_time) > timeout * 1000:
-                return None
-
-        start_time = utime.ticks_us()
-        while self.echo.value() == 1:
-            signalon = utime.ticks_us()
-            if utime.ticks_diff(signalon, start_time) > timeout * 1000:
-                return None
-
-        # Calculate the distance
-        pulse_time = utime.ticks_diff(signalon, signaloff)
-        distance = (pulse_time * 0.0343) / 2
-        return distance
+    ind = machine.Pin(0, machine.Pin.OUT)
 
 
-b = Bot(trig_pin = 16, echo_pin = 17, M1A = 8, M1B = 9,M2A = 11,M2B = 10 )
+    b.fwd()
 
-b.fwd()
-
-# state = 0
-# count = 0
-# start_time = None
+    state = 0
+    count = 0
+    start_time = None
 
 
-# while True:
+    # turn left, M1B drives the right wheel forward
+    # b.M1A.duty_u16(0) 
+    # b.M1B.duty_u16(0x4000)
+    
+    # turn right, M2B drives the left wheel forward
+    # b.M2A.duty_u16(0)
+    # b.M2B.duty_u16(0x4000)
+
+    # time.sleep_ms(1000)
+    # b.stop()
+    
+
+
+#     while True:
 #         # state machine, wait for the line to be detected, then button press.
 #         # then go straight until either sensor is 1.
 #         line = b.read_line()
@@ -176,23 +181,20 @@ b.fwd()
 #             raise(Exception(f"Invalid state ({state})"))
 #         time.sleep_ms(0)
 
-
-b.fwd()
-utime.sleep_ms(2000)
-# bot.leftRotate()
-# utime.sleep_ms(2000)
-# bot.rightRotate()
-# utime.sleep_ms(2000)
-b.brake()
-
-# while True:
-#     print(bot.read_distance())
-
-# while bot.singleSensor.value() == 0 and bot.doubleSensor1.value() == 0 and bot.doubleSensor2.value() == 0:
-#     bot.fwd()
-# while bot.singleSensor.value() == 0 and bot.doubleSensor1.value() == 1 or bot.doubleSensor2.value() == 1:
-#     bot.leftRotate()
-# while bot.singleSensor.value() == 0 and bot.doubleSensor1.value() == 0 and bot.doubleSensor2.value() == 0:
-#     bot.fwd()
-
-# bot.brake()
+# try:
+#     main()
+# except Exception as e:
+#     conf = {
+#         "M1A": 8,
+#         "M1B": 9,
+#         "M2A": 10,
+#         "M2B": 11,
+#         "left": 2,
+#         "right": 3,
+#         "A": 20,
+#         "B": 21
+#     }
+#     b = bot(**conf)
+#     b.stop()
+#     print("Emergency stop.")
+#     raise(e)
---
+++ lineTest.py
@@ -0,0 +1,12 @@
+# from olympics.ECEG341-Olympics.bot import Bot
+# import sys
+
+# bot = Bot(trig_pin = 16, echo_pin = 17, M1A = 8, M1B = 9,M2A = 11,M2B = 10 )
+
+
+# while bot.singleSensorValue == 0:
+#     bot.fwd()
+# bot.brake()
+# import os 
+
+# os.path.abspath("bot.py")
