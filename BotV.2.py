bf23297d63a91cf276fcbc50d21cf4c1d78f4791
Miguel Romero <mrm040@bucknell.edu>
2024-11-20 22:55:34 -0500

    Added lineFollowing Logic

--- BotV.2.py
+++ BotV.2.py
@@ -15,7 +15,7 @@ class bot:
         self.B = machine.Pin(kwargs["B"], machine.Pin.IN)
 
     def read_line(self):
-        return self.left.value(), self.right.value()
+        return  self.left.value(),  self.right.value()
 
     def rotate(self, speed = 0.3):
         self.M1A.duty_u16(0)     # Duty Cycle must be between 0 until 65535
@@ -93,9 +93,6 @@ def main():
 
     ind = machine.Pin(0, machine.Pin.OUT)
 
-
-    b.fwd()
-
     state = 0
     count = 0
     start_time = None
@@ -114,87 +111,87 @@ def main():
     
 
 
-#     while True:
-#         # state machine, wait for the line to be detected, then button press.
-#         # then go straight until either sensor is 1.
-#         line = b.read_line()
+    while True:
+        # state machine, wait for the line to be detected, then button press.
+        # then go straight until either sensor is 1.
+        line = b.read_line()
 
-#         if start_time is not None and time.ticks_diff(time.ticks_ms(), start_time) > 30000:
-#             print("Timeout")
-#             b.stop()
-#             state = 0
-#             start_time = None
-#             continue
+        if start_time is not None and time.ticks_diff(time.ticks_ms(), start_time) > 30000:
+            print("Timeout")
+            b.stop()
+            state = 0
+            start_time = None
+            continue
     
-#         if state == 0:
-#             if start_time is not None:
-#                 print("Run Time:", time.ticks_diff(time.ticks_ms(), start_time))
-#                 start_time = None
-#             if line == (1, 1):
-#                 print("Ready!")
-#                 state = 1
-#         elif state == 1:
-#             ind.toggle()
-#             if line != (1,1):
-#                 print("Not ready")
-#                 state = 0
-#             elif b.A.value() == 0:
-#                 while b.A.value() == 0:
-#                     time.sleep_ms(10)
-#                 count = 0
-#                 print("Start 3", end = "")
-#                 time.sleep(1)
-#                 print("2", end = "")
-#                 time.sleep(1)
-#                 print("1", end = "")
-#                 time.sleep(1)
-#                 print("Go")
-#                 state = 2
-#                 start_time = time.ticks_ms()
-#                 b.fwd(speed=0.5)
-#         elif state == 2:
-#             # on line, go forward until off the line.
+        if state == 0:
+            if start_time is not None:
+                print("Run Time:", time.ticks_diff(time.ticks_ms(), start_time))
+                start_time = None
+            if line == (1, 1):
+                print("Ready!")
+                state = 1
+        elif state == 1:
+            ind.toggle()
+            if line != (1,1):
+                print("Not ready")
+                state = 0
+            elif b.A.value() == 0:
+                while b.A.value() == 0:
+                    time.sleep_ms(10)
+                count = 0
+                print("Start 3", end = "")
+                time.sleep(1)
+                print("2", end = "")
+                time.sleep(1)
+                print("1", end = "")
+                time.sleep(1)
+                print("Go")
+                state = 2
+                start_time = time.ticks_ms()
+                b.fwd(speed=0.5)
+        elif state == 2:
+            # on line, go forward until off the line.
 
-#             if line == (0,0):
-#                 state = 3
-#         elif state == 3:
-#             # go forward until we see the line again.
-#             # steer if one sensor is on the line.
-#             if line == (1,1):
-#                 count += 1
-#                 if count == 7:
-#                     b.stop()
-#                     state = 0
-#                 else:
-#                     state = 2
-#             elif line == (1,0):
-#                 # steer left
-#                 print("LEFT")
-#                 b.turnleft(amount_u16=512)
-#             elif line == (0,1):
-#                 # steer right
-#                 print("RIGHT")
-#                 b.turnright(amount_u16=512)
-#             else:
-#                 b.fwd()
-#         else:
-#             raise(Exception(f"Invalid state ({state})"))
-#         time.sleep_ms(0)
+            if line == (0,0):
+                state = 3
+        elif state == 3:
+            # go forward until we see the line again.
+            # steer if one sensor is on the line.
+            if line == (1,1):
+                count += 1
+                if count == 7:
+                    b.stop()
+                    state = 0
+                else:
+                    state = 2
+            elif line == (1,0):
+                # steer left
+                print("LEFT")
+                b.turnleft(amount_u16=512)
+            elif line == (0,1):
+                # steer right
+                print("RIGHT")
+                b.turnright(amount_u16=512)
+            else:
+                b.fwd()
+        else:
+            raise(Exception(f"Invalid state ({state})"))
+        time.sleep_ms(0)
 
-# try:
-#     main()
-# except Exception as e:
-#     conf = {
-#         "M1A": 8,
-#         "M1B": 9,
-#         "M2A": 10,
-#         "M2B": 11,
-#         "left": 2,
-#         "right": 3,
-#         "A": 20,
-#         "B": 21
-#     }
-#     b = bot(**conf)
-#     b.stop()
-#     print("Emergency stop.")
-#     raise(e)
+try:
+    main()
+except Exception as e:
+    conf = {
+        "M1A": 8,
+        "M1B": 9,
+        "M2A": 10,
+        "M2B": 11,
+        "left": 2,
+        "right": 3,
+        "A": 20,
+        "B": 21
+    }
+    b = bot(**conf)
+    b.stop()
+    print("Emergency stop.")
+    raise(e)
---
+++ lineFollow.py
@@ -0,0 +1,93 @@
+from main import Bot
+import time
+import machine
+
+def main():
+    global b
+    global state 
+    global count
+    global start_time
+    global ind
+    b = Bot(trig_pin = 16, echo_pin = 17, M1A = 8, M1B = 9,M2A = 11,M2B = 10 , left_sensor = 27, right_sensor = 26)
+
+    ind = machine.Pin(0, machine.Pin.OUT)
+
+    state = 0
+    count = 0
+    start_time = None
+
+try:
+    main()
+except Exception as e:
+    b = Bot(trig_pin = 16, echo_pin = 17, M1A = 8, M1B = 9,M2A = 11,M2B = 10 , left_sensor = 27, right_sensor = 26)
+    b.stop()
+    print("Emergency stop.")
+    raise(e)
+while True:
+    # state machine, wait for the line to be detected, then button press.
+    # then go straight until either sensor is 1.
+    line = b.read_line()
+
+    if start_time is not None and time.ticks_diff(time.ticks_ms(), start_time) > 30000:
+        print("Timeout")
+        b.stop()
+        state = 0
+        start_time = None
+        continue
+
+    if state == 0:
+        if start_time is not None:
+            print("Run Time:", time.ticks_diff(time.ticks_ms(), start_time))
+            start_time = None
+        if line == (1, 1):
+            print("Ready!")
+            state = 1
+    elif state == 1:
+        ind.toggle()
+        if line != (1,1):
+            print("Not ready")
+            state = 0
+        elif b.A.value() == 0:
+            while b.A.value() == 0:
+                time.sleep_ms(10)
+            count = 0
+            print("Start 3", end = "")
+            time.sleep(1)
+            print("2", end = "")
+            time.sleep(1)
+            print("1", end = "")
+            time.sleep(1)
+            print("Go")
+            state = 2
+            start_time = time.ticks_ms()
+            b.fwd(speed=0.5)
+    elif state == 2:
+        # on line, go forward until off the line.
+
+        if line == (0,0):
+            state = 3
+    elif state == 3:
+        # go forward until we see the line again.
+        # steer if one sensor is on the line.
+        if line == (1,1):
+            count += 1
+            if count == 7:
+                b.stop()
+                state = 0
+            else:
+                state = 2
+        elif line == (1,0):
+            # steer left
+            print("LEFT")
+            b.turnleft(amount_u16=512)
+        elif line == (0,1):
+            # steer right
+            print("RIGHT")
+            b.turnright(amount_u16=512)
+        else:
+            b.fwd()
+    else:
+        raise(Exception(f"Invalid state ({state})"))
+    time.sleep_ms(0)
+
+
