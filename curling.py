b1d6cf19438ce0a2ac661bc2010095c712028989
Mike Merola <mfm024@bucknell.edu>
2024-12-04 14:38:18 -0500

    curling remove distance

--- curling.py
+++ curling.py
@@ -11,37 +11,6 @@ n[0] = (255, 0, 0)  # Set the first LED to red
 n[1] = (255, 0, 0)  # Set the second LED to green
 
 
-class mydistance:
-    def __init__(self, echo, trigger, period_ms=100, buffer_size=10):  
-        self.echo = echo
-        self.trigger = trigger
-        self.buffer = array('I', [0] * buffer_size)
-        self.idx = 0
-        self.period_ms = period_ms
-
-    async def task(self):
-        while True:
-            self.trigger.low()
-            utime.sleep_us(2)
-            self.trigger.high()
-            utime.sleep_us(5)
-            self.trigger.low()
-            while self.echo.value() == 0:
-                signaloff = utime.ticks_us()
-            while self.echo.value() == 1:
-                signalon = utime.ticks_us()
-            timepassed = signalon - signaloff
-
-            val = timepassed
-            self.buffer[self.idx] = val
-            self.idx = (self.idx + 1) % len(self.buffer)
-            await asyncio.sleep_ms(self.period_ms)
-
-def get_distance(timepassed):
-    distance = (timepassed * 0.0343)/2
-    return distance
-
-
 async def main():
     global bot
     conf ={
@@ -59,12 +28,7 @@ async def main():
     bot = Bot(**conf)
     p = Pin(18)
     n = neopixel.NeoPixel(p,32)
-    # n[0] = (255, 0, 0)  # Set the first LED to red
-    # n[1] = (255, 0, 0)  # Set the second LED to red
-    # n.write()
     while True:
-        await asyncio.sleep_ms(100)
-        loop = asyncio.get_event_loop()
         distance = bot.read_distance()
         print(distance)
         #needs to change based on the distance the wall will be from the target
@@ -79,6 +43,3 @@ async def main():
             n[1] = (255, 0, 0)  # Set the second LED to red
             n.write()
             bot.fwd()
-
-
-asyncio.run(main())
